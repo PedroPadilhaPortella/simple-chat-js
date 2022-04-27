52eed9af2c5d2221e469d3f45430219163aa150b
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-04-27 00:31:29 -0300

    socket io logic and stream

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express')
 const http = require('http')
 
 const port = 3000
+const users = []
+
 const app = express()
 const server = http.Server(app)
 const io = require('socket.io')(server)
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -14,11 +17,18 @@ app.get('/styles/index.css', (req, res) => {
     res.sendFile(__dirname + '/styles/index.css')
 })
 
-io.on('connection', (socket) => {   
-    console.log('a user has been connected')
+io.on('connection', (socket) => {
+    let name = ''
+
+    socket.on('has connected', (username) => {
+        name = username
+        users.push(username)
+        io.emit('has connected', {users, username: name})
+    })
 
     socket.on('disconnect', () => {
-        console.log('a user has been disconnected')
+        users.splice(users.indexOf(name), 1)
+        io.emit('has disconnected', {users, username: name})
     })
 })
 
